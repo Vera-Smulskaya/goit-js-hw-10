5a45e2b0463c55a864446c60bc26354800e67900
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-09-11 22:01:01 +0300

    add cat-api.js and create two functions for getting data from cat-api

---
+++ src/cat-api.js
@@ -0,0 +1,26 @@
+const api_key =
+  'live_vxATxzZ9RoqZ86tp4QZ3XU7w8KZcPO7iT8k0l0xfJIpuGF7QNm4uQN6y9QdUDbTb';
+
+export function fetchBreeds() {
+  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${api_key}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      console.log(response.json());
+      return response.json();
+    }
+  );
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?api_key=${api_key}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    console.log(response.json());
+    return response.json();
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_vxATxzZ9RoqZ86tp4QZ3XU7w8KZcPO7iT8k0l0xfJIpuGF7QNm4uQN6y9QdUDbTb';
