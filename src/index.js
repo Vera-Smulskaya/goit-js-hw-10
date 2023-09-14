b04656e222ce64cf461c317d6e9c0ac639fe67dc
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-09-14 21:07:28 +0300

    install Slimselectjs and Notifix. Add styles.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 // import axios from 'axios';
+import './styles.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SlimSelect from 'slim-select';
+// import 'slim-select/dist/slimselect.css';
 
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_vxATxzZ9RoqZ86tp4QZ3XU7w8KZcPO7iT8k0l0xfJIpuGF7QNm4uQN6y9QdUDbTb';
+axios.defaults.headers.common['x-api-key'] =
+  'live_vxATxzZ9RoqZ86tp4QZ3XU7w8KZcPO7iT8k0l0xfJIpuGF7QNm4uQN6y9QdUDbTb';
 
 fetchBreeds();
 fetchCatByBreed('abys');
