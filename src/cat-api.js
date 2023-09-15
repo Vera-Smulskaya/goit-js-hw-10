9ff973be7425e5523464fe8a0fc4dade1fff311d
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-09-15 16:35:49 +0300

    add logic of rendering cats and description to the page

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,20 +1,19 @@
 const api_key =
   'live_vxATxzZ9RoqZ86tp4QZ3XU7w8KZcPO7iT8k0l0xfJIpuGF7QNm4uQN6y9QdUDbTb';
+const BASE_URL = 'https://api.thecatapi.com/v1/';
 
-export function fetchBreeds() {
-  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${api_key}`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
+export function fetchCats() {
+  return fetch(`${BASE_URL}breeds?api_key=${api_key}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
-  );
+    return response.json();
+  });
 }
 
-export function fetchCatByBreed(breedId) {
+export function fetchCatBreed(breedId) {
   return fetch(
-    `https://api.thecatapi.com/v1/images/search?api_key=${api_key}&breed_ids=${breedId}`
+    `${BASE_URL}images/search?api_key=${api_key}&breed_ids=${breedId}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-// import axios from 'axios';
+import { fetchCats, fetchCatBreed } from './cat-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 import './styles.css';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import SlimSelect from 'slim-select';
-// import 'slim-select/dist/slimselect.css';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_vxATxzZ9RoqZ86tp4QZ3XU7w8KZcPO7iT8k0l0xfJIpuGF7QNm4uQN6y9QdUDbTb';
-
-fetchBreeds();
-fetchCatByBreed('abys');
 
 const selector = document.querySelector('.breed-select');
 const catInfoEl = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
+
+catInfoEl.classList.add('is-hidden');
+loader.classList.replace('loader', 'is-hidden');
+error.classList.add('is-hidden');
+
+selector.addEventListener('change', onSelect);
+
+let arrBreeds = [];
+fetchCats()
+  .then(data => {
+    data.forEach(element => {
+      arrBreeds.push({ text: element.name, value: element.id });
+    });
+    new SlimSelect({
+      select: selector,
+      data: arrBreeds,
+    });
+  })
+  .catch(onCatchError);
+
+function onSelect(event) {
+  loader.classList.replace('is-hidden', 'loader');
+  selector.classList.add('is-hidden');
+  catInfoEl.classList.add('is-hidden');
+
+  const breedId = event.currentTarget.value;
+  fetchCatBreed(breedId)
+    .then(data => {
+      loader.classList.replace('loader', 'is-hidden');
+      selector.classList.remove('is-hidden');
+      const { url, breeds } = data[0];
+
+      catInfoEl.innerHTML = `<div class="cat-info__img">
+      <img src="${url}" alt="${breeds[0].name}" width="600"/>
+      </div>
+      <div class="cat-info__desc">
+      <h1>${breeds[0].name}</h1>
+      <h2>Description:</h2>
+      <p>${breeds[0].description}</p>
+      <h2>Temperament:</h2>
+      <p>${breeds[0].temperament}</p>
+      </div>`;
+      catInfoEl.classList.remove('is-hidden');
+    })
+    .catch(onCatchError);
+}
+
+function onCatchError() {
+  selector.classList.remove('is-hidden');
+  loader.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+    {
+      position: 'center-center',
+      timeout: 3000,
+      width: '400px',
+      fontSize: '16px',
+    }
+  );
+}
